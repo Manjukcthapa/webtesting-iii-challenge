59a20cee83cbc6f1dcbaec519cae868501063a84
Manju KC <kcmanjuthapa@gmail.com>
2019-10-14 16:02:20 -0400

    Dashboard test completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import { render, cleanup } from 'react-testing-library'
+import 'jest-dom/extend-expect';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+afterEach(cleanup)
+
+it('renders without crashing', () => {
+    render(<Display />);
+    render(<Controls />);
+
+});
